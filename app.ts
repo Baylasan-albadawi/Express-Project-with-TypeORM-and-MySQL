b4d124f0a036a52829fed58daab08ccee412d520
Baylasan Albadawi <211169@ppu.edu.ps>
2024-07-27 02:16:27 +0300

    PORT number hiding

--- app.ts
+++ app.ts
@@ -6,7 +6,7 @@ import { customErrorHandler } from "./middleware/errorHandeller.js";
 import dataSource from "./db/dbConfig.js"
 
 const app: Express = express();
-const PORT =  3000
+const PORT = process.env.PORT || 3000
 
 
 app.use(express.json())
